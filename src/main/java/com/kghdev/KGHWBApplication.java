e6cb95bc97789e9fb7d95d460986b3d5a2e44ac0
kgh <gihyeon6394@gmail.com>
2023-01-03 13:59:00 +0900

    first commit

---
+++ src/main/java/com/kghdev/KGHWBApplication.java
@@ -0,0 +1,12 @@
+package com.kghdev;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KGHWBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KGHWBApplication.class, args);
+    }
+}
