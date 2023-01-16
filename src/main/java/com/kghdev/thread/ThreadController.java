4e6a366e4277d85ae57e77160c6aa82fcd51bdd6
kgh <gihyeon6394@gmail.com>
2023-01-16 16:02:16 +0900

    Thread 공부 시작

---
+++ src/main/java/com/kghdev/thread/ThreadController.java
@@ -0,0 +1,11 @@
+package com.kghdev.thread;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/thr")
+public class ThreadController {
+
+
+}
