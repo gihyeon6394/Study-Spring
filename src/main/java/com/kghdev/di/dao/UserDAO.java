dd868bad554e112ab62a6d64c9df310bbb2ef504
gimgihyeon <gihyeon6394@gmail.com>
2023-02-21 14:01:24 +0900

    Object의 identity, equality

--- src/main/java/com/kghdev/di/dao/UserDAO.java
+++ src/main/java/com/kghdev/di/dao/UserDAO.java
@@ -1,8 +1,18 @@
 package com.kghdev.di.dao;
 
+import com.kghdev.di.vo.Car;
+
 public class UserDAO {
 
 
+    public static void main(String[] args){
+        /**
+         * Object의 identity, equality
+         * */
+        Car bmw1 = new Car("x1", "bmw");
+        Car bmw2 = new Car("x1", "bmw");
+        System.out.println(bmw1.equals(bmw2));
 
 
+    }
 }
