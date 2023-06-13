36ffe5e66054ccc3e533d4d1e2cd5d9b5bcb0c75
gimgihyeon <gihyeon6394@gmail.com>
2023-06-13 21:52:25 +0900

    feat : 6.1.1 메소드 분리

---
+++ src/main/java/com/tob/part6/UserServiceTest.java
@@ -0,0 +1,132 @@
+package com.tob.part6;
+
+import com.tob.part5.MockMailSender;
+import com.tob.part5.dao.JDBCTemplateDAO;
+import com.tob.part5.service.UserService;
+import com.tob.part5.vo.Level;
+import com.tob.part5.vo.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.tob.part5.service.UserService.MIN_LOGIN_COUNT_FOR_SILVER;
+import static com.tob.part5.service.UserService.MIN_RECOMMEND_COUNT_FOR_GOLD;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("/applicationContext.xml")
+public class UserServiceTest {
+
+
+    @Autowired
+    private ApplicationContext ac;
+
+    @Autowired
+    private UserService userService;
+
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+
+    @Autowired
+    private MailSender mailSender;
+
+    private JDBCTemplateDAO userDao;
+
+
+    private List<User> userList;
+
+    private User user;
+
+    public static void main(String args[]) throws SQLException, ClassNotFoundException {
+
+        JUnitCore.main("com.tob.part6.UserServiceTest");
+    }
+
+    /**
+     * UserSerivce가 bean에 잘 등록되었는지 확인
+     */
+    @Test
+    public void bean() {
+        assertThat(this.userService, is(notNullValue()));
+    }
+
+    @Before
+    public void setUp() {
+        userDao = this.ac.getBean("jdbcTemplateDAO5", JDBCTemplateDAO.class); // getBean() : Dependency lookup
+        dataSource = this.ac.getBean("dataSource", DataSource.class);
+        // 경계값을 사용하는 테스트
+        userList = Arrays.asList(new User.Builder().name("카리나").nameGroup("에스파").level(Level.GOLD).cntLogin(MIN_LOGIN_COUNT_FOR_SILVER - 10).cntRecommend(MIN_RECOMMEND_COUNT_FOR_GOLD - 10).email("karina@email.com").build()
+                , new User.Builder().name("지수").nameGroup("블랙핑크").level(Level.SILVER).cntLogin(MIN_LOGIN_COUNT_FOR_SILVER).cntRecommend(MIN_RECOMMEND_COUNT_FOR_GOLD).build()
+                , new User.Builder().name("아이린").nameGroup("레드벨벳").level(Level.BASIC).cntLogin(MIN_LOGIN_COUNT_FOR_SILVER - 1).cntRecommend(MIN_RECOMMEND_COUNT_FOR_GOLD - 20).build()
+                , new User.Builder().name("쯔위").nameGroup("트와이스").level(Level.BASIC).cntLogin(MIN_LOGIN_COUNT_FOR_SILVER).cntRecommend(MIN_RECOMMEND_COUNT_FOR_GOLD).email("zzzz@email.com").build()
+                , new User.Builder().name("수지").nameGroup("미쓰에이").level(Level.BASIC).cntLogin(MIN_LOGIN_COUNT_FOR_SILVER).cntRecommend(MIN_RECOMMEND_COUNT_FOR_GOLD).build()
+                , new User.Builder().name("아이유").nameGroup("솔로").level(Level.BASIC).cntLogin(MIN_LOGIN_COUNT_FOR_SILVER).cntRecommend(MIN_RECOMMEND_COUNT_FOR_GOLD).build());
+
+        user = new User.Builder().name("테스트").nameGroup("테스트").level(Level.BASIC).cntLogin(1).cntRecommend(1).build();
+    }
+
+    private void checkLevel(User user, Level levelExpected) {
+        User userUpdate = userDao.selectByName(user.getName());
+        assertThat(userUpdate.getLevel(), is(levelExpected));
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User userUpdate = userDao.selectByName(user.getName());
+        if (upgraded) {
+            assertThat(userUpdate.getLevel(), is(user.getLevel().getNextLevel()));
+        } else {
+            assertThat(userUpdate.getLevel(), is(user.getLevel()));
+        }
+    }
+
+    @Test
+    @DirtiesContext
+    public void upgradeAllOrNothing() throws SQLException {
+        UserService testUserService = new UserService.TestUserService(userList.get(3).getName());
+
+        testUserService.setUserDao(userDao);
+
+        MockMailSender mockMailSender = new MockMailSender();
+        testUserService.setMailSender(mockMailSender);
+
+        testUserService.setTransactionManager(transactionManager);
+
+        userDao.deleteAll();
+        for (User user : userList) {
+            userDao.add(user);
+        }
+        try {
+            testUserService.upgradeLevels();
+        } catch (UserService.TestUserService.TestUserServiceException e) {
+
+        }
+        checkLevelUpgraded(userList.get(1), false);
+
+        List<String> request = mockMailSender.getRequests();
+
+        assertThat(request.size(), greaterThan(0));
+
+    }
+
+
+}
